8749b3c03d796a6955f222c4098f0e380a9a7418
Mike Wallio <michael.wallio@outlook.com>
2025-10-03 13:49:31 -0400

    Cleanup and sidekick

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,36 +2,5 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = true },
-    servers = {
-      typescript = {
-        inlayHints = {
-          includeInlayParameterNameHints = "all",
-          includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-          includeInlayFunctionParameterTypeHints = true,
-          includeInlayVariableTypeHints = true,
-          includeInlayPropertyDeclarationTypeHints = true,
-          includeInlayFunctionLikeReturnTypeHints = true,
-          includeInlayEnumMemberValueHints = true,
-        },
-        javascript = {
-          inlayHints = {
-            includeInlayParameterNameHints = "all",
-            includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-            includeInlayVariableTypeHints = true,
-            includeInlayFunctionParameterTypeHints = true,
-            includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-            includeInlayPropertyDeclarationTypeHints = true,
-            includeInlayFunctionLikeReturnTypeHints = true,
-            includeInlayEnumMemberValueHints = true,
-          },
-        },
-      },
-    },
-    setup = {
-      tsserver = function(_, opts)
-        require("typescript").setup({ server = opts })
-        return true
-      end,
-    },
   },
 }
