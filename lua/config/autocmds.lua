0f1c245232c29e52cc3bfbfe886ea84358450ed1
Mike Wallio <michael.wallio@outlook.com>
2025-12-12 16:50:25 -0500

    Reduce transparencies

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,20 +1,8 @@
 local function set_transparency()
   vim.cmd([[
     hi Normal guibg=NONE ctermbg=NONE
-    hi NonText guibg=NONE ctermbg=NONE
-    hi NormalFloat guibg=NONE ctermbg=NONE
     hi NormalNC guibg=NONE ctermbg=NONE
     hi SignColumn guibg=NONE ctermbg=NONE
-    hi StatusLine guibg=NONE ctermbg=NONE
-    hi StatusLineNC guibg=NONE ctermbg=NONE
-    hi VertSplit guibg=NONE ctermbg=NONE
-    hi TabLine guibg=NONE ctermbg=NONE
-    hi TabLineFill guibg=NONE ctermbg=NONE
-    hi TabLineSel guibg=NONE ctermbg=NONE
-    hi Pmenu guibg=NONE ctermbg=NONE
-    hi PmenuSel guibg=NONE ctermbg=NONE
-    hi NeoTreeNormal guibg=NONE ctermbg=NONE
-    hi NeoTreeNormalNC guibg=NONE ctermbg=NONE
   ]])
 end
 
