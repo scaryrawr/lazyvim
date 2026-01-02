d5d5efb1dac3de388bbb418000562240e924725b
Mike Wallio <michael.wallio@outlook.com>
2026-01-02 10:30:56 -0500

    Change theme to ansi

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "jeffkreeftmeijer/vim-dim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dim",
+    },
+  },
+}
