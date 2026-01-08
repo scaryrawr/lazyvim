56b431d731455856eee1a7b50a7599f56e3bca71
Mike Wallio <michael.wallio@outlook.com>
2026-01-08 12:55:32 -0500

    Add variations of launching copilot

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -7,7 +7,26 @@ return {
           cmd = { "magus" },
           url = "https://github.com/scaryrawr/magus",
         },
-        codex = { cmd = { "codex" } },
+        yopilot = {
+          cmd = { "copilot", "--allow-all-tools", "--allow-all-paths" },
+          url = "https://github.com/github/copilot-cli",
+        },
+        topilot = {
+          cmd = { "copilot", "--allow-all-tools" },
+          url = "https://github.com/github/copilot-cli",
+        },
+        ropilot = {
+          cmd = { "copilot", "--resume" },
+          url = "https://github.com/github/copilot-cli",
+        },
+        mopilot = {
+          cmd = { "copilot", "--disable-builtin-mcps" },
+          url = "https://github.com/github/copilot-cli",
+        },
+        codex = {
+          cmd = { "codex" },
+          url = "https://github.com/openai/codex",
+        },
         pi = {
           cmd = { "pi" },
           url = "https://github.com/badlogic/pi-mono/",
