83f23c5f1c9d2a503f2e03092b1eb3f184360634
Mike Wallio <michael.wallio@outlook.com>
2026-03-08 20:35:32 -0400

    Back to catppuccin

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,26 +1,30 @@
 return {
   {
-    "ellisonleao/gruvbox.nvim",
-    name = "gruvbox",
+    "catppuccin/nvim",
+    name = "catppuccin",
     opts = {
-      terminal_colors = true,
-      transparent_mode = true,
+      flavour = "auto",
+      background = {
+        light = "latte",
+        dark = "mocha",
+      },
+      transparent_background = true,
     },
   },
   {
     "LazyVim/LazyVim",
     init = function()
-      vim.api.nvim_create_user_command("GruvboxLight", function()
+      vim.api.nvim_create_user_command("CatppuccinLight", function()
         vim.o.background = "light"
-        vim.cmd.colorscheme("gruvbox")
-      end, { desc = "Use Gruvbox light mode" })
-      vim.api.nvim_create_user_command("GruvboxDark", function()
+        vim.cmd.colorscheme("catppuccin")
+      end, { desc = "Use Catppuccin light mode" })
+      vim.api.nvim_create_user_command("CatppuccinDark", function()
         vim.o.background = "dark"
-        vim.cmd.colorscheme("gruvbox")
-      end, { desc = "Use Gruvbox dark mode" })
+        vim.cmd.colorscheme("catppuccin")
+      end, { desc = "Use Catppuccin dark mode" })
     end,
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "catppuccin",
     },
   },
 }
